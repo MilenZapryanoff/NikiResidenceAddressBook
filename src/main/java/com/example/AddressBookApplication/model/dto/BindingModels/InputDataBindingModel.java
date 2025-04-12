3119d7b685ad8d6fa58f2b82293a9fe1f26d5867
MilenZapryanoff <milen.zapryanov@gmail.com>
2025-04-12 13:12:58 +0300

    Repo Init

---
+++ src/main/java/com/example/AddressBookApplication/model/dto/BindingModels/InputDataBindingModel.java
@@ -0,0 +1,247 @@
+package com.example.AddressBookApplication.model.dto.BindingModels;
+
+public class InputDataBindingModel {
+
+    private String propertyNumber;
+
+    private String ownerName;
+    private String ownerPhone;
+    private String ownerEmail;
+
+    private String adult1Name;
+    private String adult1Phone;
+    private String adult1Email;
+
+    private String adult2Name;
+    private String adult2Phone;
+
+    private String adult3Name;
+    private String adult3Phone;
+
+    private String adult4Name;
+    private String adult4Phone;
+
+    private String adult5Name;
+    private String adult5Phone;
+
+    private String child1Name;
+    private String child1Age;
+    private String child2Name;
+    private String child2Age;
+    private String child3Name;
+    private String child3Age;
+    private String child4Name;
+    private String child4Age;
+    private String child5Name;
+    private String child5Age;
+
+    private Long numberOfPets;
+
+    public String getPropertyNumber() {
+        return propertyNumber;
+    }
+
+    public void setPropertyNumber(String propertyNumber) {
+        this.propertyNumber = propertyNumber;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getOwnerPhone() {
+        return ownerPhone;
+    }
+
+    public void setOwnerPhone(String ownerPhone) {
+        this.ownerPhone = ownerPhone;
+    }
+
+    public String getOwnerEmail() {
+        return ownerEmail;
+    }
+
+    public void setOwnerEmail(String ownerEmail) {
+        this.ownerEmail = ownerEmail;
+    }
+
+    public String getAdult1Name() {
+        return adult1Name;
+    }
+
+    public void setAdult1Name(String adult1Name) {
+        this.adult1Name = adult1Name;
+    }
+
+    public String getAdult1Phone() {
+        return adult1Phone;
+    }
+
+    public void setAdult1Phone(String adult1Phone) {
+        this.adult1Phone = adult1Phone;
+    }
+
+    public String getAdult2Name() {
+        return adult2Name;
+    }
+
+    public void setAdult2Name(String adult2Name) {
+        this.adult2Name = adult2Name;
+    }
+
+    public String getAdult2Phone() {
+        return adult2Phone;
+    }
+
+    public void setAdult2Phone(String adult2Phone) {
+        this.adult2Phone = adult2Phone;
+    }
+
+    public String getAdult3Name() {
+        return adult3Name;
+    }
+
+    public void setAdult3Name(String adult3Name) {
+        this.adult3Name = adult3Name;
+    }
+
+    public String getAdult3Phone() {
+        return adult3Phone;
+    }
+
+    public void setAdult3Phone(String adult3Phone) {
+        this.adult3Phone = adult3Phone;
+    }
+
+    public String getAdult4Name() {
+        return adult4Name;
+    }
+
+    public void setAdult4Name(String adult4Name) {
+        this.adult4Name = adult4Name;
+    }
+
+    public String getAdult4Phone() {
+        return adult4Phone;
+    }
+
+    public void setAdult4Phone(String adult4Phone) {
+        this.adult4Phone = adult4Phone;
+    }
+
+    public String getAdult5Name() {
+        return adult5Name;
+    }
+
+    public void setAdult5Name(String adult5Name) {
+        this.adult5Name = adult5Name;
+    }
+
+    public String getAdult5Phone() {
+        return adult5Phone;
+    }
+
+    public void setAdult5Phone(String adult5Phone) {
+        this.adult5Phone = adult5Phone;
+    }
+
+    public String getChild1Name() {
+        return child1Name;
+    }
+
+    public void setChild1Name(String child1Name) {
+        this.child1Name = child1Name;
+    }
+
+    public String getChild2Name() {
+        return child2Name;
+    }
+
+    public void setChild2Name(String child2Name) {
+        this.child2Name = child2Name;
+    }
+
+    public String getChild3Name() {
+        return child3Name;
+    }
+
+    public void setChild3Name(String child3Name) {
+        this.child3Name = child3Name;
+    }
+
+    public String getChild4Name() {
+        return child4Name;
+    }
+
+    public void setChild4Name(String child4Name) {
+        this.child4Name = child4Name;
+    }
+
+    public String getChild5Name() {
+        return child5Name;
+    }
+
+    public void setChild5Name(String child5Name) {
+        this.child5Name = child5Name;
+    }
+
+    public Long getNumberOfPets() {
+        return numberOfPets;
+    }
+
+    public void setNumberOfPets(Long numberOfPets) {
+        this.numberOfPets = numberOfPets;
+    }
+
+    public String getAdult1Email() {
+        return adult1Email;
+    }
+
+    public void setAdult1Email(String adult1Email) {
+        this.adult1Email = adult1Email;
+    }
+
+    public String getChild1Age() {
+        return child1Age;
+    }
+
+    public void setChild1Age(String child1Age) {
+        this.child1Age = child1Age;
+    }
+
+    public String getChild2Age() {
+        return child2Age;
+    }
+
+    public void setChild2Age(String child2Age) {
+        this.child2Age = child2Age;
+    }
+
+    public String getChild3Age() {
+        return child3Age;
+    }
+
+    public void setChild3Age(String child3Age) {
+        this.child3Age = child3Age;
+    }
+
+    public String getChild4Age() {
+        return child4Age;
+    }
+
+    public void setChild4Age(String child4Age) {
+        this.child4Age = child4Age;
+    }
+
+    public String getChild5Age() {
+        return child5Age;
+    }
+
+    public void setChild5Age(String child5Age) {
+        this.child5Age = child5Age;
+    }
+}
